cfbc498a64a334a6a48fea8dda9a4fd4219860c9
Emanuele19 <emanuele190403@gmail.com>
2026-01-23 17:07:44 +0000

    update

--- dataset_module/__init__.py
+++ dataset_module/__init__.py
@@ -1,4 +1,5 @@
 from .fasta_dataset import FastaDataset, FastaContent
 from .FastaWindowDataset import FastaWindowDataset
+from .encoding import SequenceEncoder
 
-__all__ = [FastaContent, FastaDataset, FastaWindowDataset]
+__all__ = [FastaContent, FastaDataset, FastaWindowDataset, SequenceEncoder]
